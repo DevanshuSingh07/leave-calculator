02cf098b8458f92456f4c7d997af2423add6cc3b
DevanshuSingh07 <127047398+DevanshuSingh07@users.noreply.github.com>
2024-07-11 23:27:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+// Create connection to MySQL database
+const connection = mysql.createConnection({
+  host: "sql12.freesqldatabase.com",
+  user: "sql12719187",
+  password: "v4RUBhGlQ5",
+  database: "sql12719187",
+});
+
+const app = express();
+const port = 3000;
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+//Example API endpoint to fetch data
+app.get("/api", (req, res) => {
+  const value = req.header("need");
+  const sql = `SELECT days FROM records WHERE Mcondition = '${value}';`;
+  console.log(sql);
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
